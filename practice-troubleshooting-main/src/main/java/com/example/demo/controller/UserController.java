63e9e5bc51daaf9771eb79dfe955bb74eddb7835
incheol617 <54878414+incheol617@users.noreply.github.com>
2024-11-28 11:42:29 +0900

    Add files via upload

---
+++ practice-troubleshooting-main/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api/v1/user")
+@RestController
+public class UserController {
+
+  @GetMapping
+  public ResponseEntity<String> getEmployeeNumber() {
+    String employeeNumber = "82265825";
+    log.info("Respond with employee number {}", employeeNumber);
+
+    return new ResponseEntity<>(employeeNumber, HttpStatus.OK);
+  }
+
+}
